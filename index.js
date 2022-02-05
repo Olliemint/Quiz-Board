2aafeef54a9324ec8cee8e74e2cb2cb3f5d5d13d
Olliemint <furywrld5@gmail.com>
2022-02-05 15:11:20 +0300

    fix:linked css and js to html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function check() {
     var c=0;
-    var Q1=document.getElementById.Questions.question1.value;
-    if (Q1=="File") {c++}
+    var q1=document.quiz.question1.value;
+    if (q1=="File") {c++}
     document.write(c);
 }
