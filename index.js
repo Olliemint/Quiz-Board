865e5b4683d5f7067d44dda8265c74dc8c628ad3
Olliemint <furywrld5@gmail.com>
2022-02-05 18:12:12 +0300

    improved function

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 function check() {
     var c=0;
-    var q1=document.quiz.question1.value;
-    if (q1=="File") {c++}
+    var Q1=document.quiz.question1.value;
+    var Q2=document.quiz.question2.value;
+    var Q3=document.quiz.question3.value;
+    var Q4=document.quiz.question4.value;
+    var Q5=document.quiz.question5.value;
+    var Q6=document.quiz.question6.value;
+    var Q7=document.quiz.question7.value;
+    var Q8=document.quiz.question8.value;
+    var Q9=document.quiz.question9.value;
+    
+    if (Q1=="File") {c++}
+    if (Q2=="File") {c++}
+    if (Q3=="File") {c++}
+    if (Q4=="File") {c++}
+    if (Q5=="File") {c++}
+    if (Q6=="File") {c++}
+    if (Q7=="File") {c++}
+    if (Q8=="File") {c++}
+    if (Q9=="File") {c++}
+
     document.write(c);
 }
