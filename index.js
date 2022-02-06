31709c186a0dbf37e60abedf1f26572bc9a9f19b
Olliemint <furywrld5@gmail.com>
2022-02-07 00:10:46 +0300

    feat:quiz.html

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function check() {
     var Q3=document.quiz.question3.value;
     var Q4=document.quiz.question4.value;
     var Q5=document.quiz.question5.value;
-    var Q6=document.quiz.question6.value;
-    var Q7=document.quiz.question7.value;
-    var Q8=document.quiz.question8.value;
-    var Q9=document.quiz.question9.value;
+    //var Q6=document.quiz.question6.value;
+   // var Q7=document.quiz.question7.value;
+   // var Q8=document.quiz.question8.value;
+    //var Q9=document.quiz.question9.value;
     var results=document.getElementById('results');
     var quiz=document.getElementById("quiz");
     if (Q1=="File") {c++}
@@ -16,10 +16,10 @@ function check() {
     if (Q3=="All of the above") {c++}
     if (Q4=="Conditional operator") {c++}
     if (Q5=="Mocha") {c++}
-    if (Q6=="Cassandra") {c++}
-    if (Q7=="use strict") {c++}
-    if (Q8=="Object") {c++}
-    if (Q9=="Graphics") {c++}
+   // if (Q6=="Cassandra") {c++}
+    //if (Q7=="use strict") {c++}
+    //if (Q8=="Object") {c++}
+    //if (Q9=="Graphics") {c++}
 
     quiz.style.display="none";
     
